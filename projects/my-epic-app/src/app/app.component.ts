fdaac55aa1cd0e1970ac77335c89022a4c330da8
Ashish Mishra <ashishmishra221984@gmail.com>
2020-10-11 14:02:00 +0530

    added oninit interface

--- projects/my-epic-app/src/app/app.component.ts
+++ projects/my-epic-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-epic-app';
 }
